21183321d28db9e5e9682b2aeb3c99e01a7442db
Léo Meillier <l.meillier09@gmail.com>
2022-04-05 17:07:07 +0200

    Update vitta_esp32.py

--- vitta_esp32.py
+++ vitta_esp32.py
@@ -42,3 +42,54 @@ class CMD:
     
     def respond(self, cmd, status=1, value=None):
         return "{\"cmd\":\"" + str(cmd).replace('"', '\\"')+ "\", \"status\":" + str(status) + ", \"value\":" + ("null" if value is None else str(value)) + "}\n"
+    
+    def connect_station(self, ssid='', password='', ip='', mask='', gateway=''):
+        self.station = network.WLAN(network.STA_IF)
+        if self.station.isconnected():
+            if self.station.config('essid') is ssid:
+                print("Already connected on ssid: '%s'" % self.station.config('essid'))
+                return
+            else:
+                disconnect_station()
+        print("\nTrying to connect to '%s' ..." % ssid)
+        if len(ip) is not 0:
+            if len(gateway) == 0:
+                gateway = ip.split('.')[0] + '.' + ip.split('.')[1] + '.' + ip.split('.')[2] + '.1'
+            if len(mask) == 0:
+                mask = '255.255.255.0'
+            self.station.ifconfig([ip, mask, gateway, gateway])
+        if not self.station.active():
+            self.station.active(True)
+        self.station.connect(ssid, password)
+        while not self.station.isconnected():
+            pass
+        print("Station connected !")
+
+    def disconnect_station(self):
+        if self.station is not None and self.station.isconnected():
+            ssid = self.station.config('essid')
+            self.station.disconnect()
+            for retry in range(100):
+                connected = self.station.isconnected()
+                if not connected:
+                    break
+                utime.sleep(0.1)
+            if not connected:
+                self.station.active(False)
+                utime.sleep(0.2)
+                print("Disconnected from '%s'\n" %ssid)
+            else:
+                print("Disconnection from '%s' failed.\n" %ssid)
+        else:
+            print("Station already disconnected.\n")
+        
+    def configure_access_point(self, ssid='', ip='', activate=True, max_clients=50):
+        self.ap = network.WLAN(network.AP_IF)
+        if len(ip) is not 0:
+            gateway = ip.split('.')[0] + '.' + ip.split('.')[1] + '.' + ip.split('.')[2] + '.1'
+            self.ap.ifconfig([ip, '255.255.255.0', gateway, gateway])
+        self.ap.active(activate)
+        self.ap.config(essid=ssid, password='')
+        self.ap.config(max_clients=max_clients)
+        print("Access point started.\n")
+        return self.ap
